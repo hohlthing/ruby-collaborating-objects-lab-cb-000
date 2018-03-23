d193d4b5cc4344321fb7a9dfa0a801f8f6168963
michael hohl <mhohl@rocketmail.com>
2018-03-23 17:35:39 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,12 @@
 class Artist
 
   attr_accessor :name
+  attr_reader :songs
+  @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
 end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+
+  attr_reader :path, :files
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+
+  end
+
+  def import
+#    Song.new_by_filename(some_filename)
+  end
+
+end
