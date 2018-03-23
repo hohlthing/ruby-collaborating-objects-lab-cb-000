ae040f9535b02cac2ac62c579fbd9f9881ba4541
michael hohl <mhohl@rocketmail.com>
2018-03-23 17:47:21 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,4 +21,16 @@ class Artist
     @@all
   end
 
+  def self.create_by_name(name)
+    artist = Artist.new(name)
+  end
+
+  def self.find_by_name(name)
+    self.all.detect { |x| x.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
 end
