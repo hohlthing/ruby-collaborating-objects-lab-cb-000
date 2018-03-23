7fdabc1d4f9a0a06a09d4abaae967b84ca2ce8ad
michael hohl <mhohl@rocketmail.com>
2018-03-23 17:51:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,4 +6,13 @@ attr_accessor :name, :artist
     @name = name
   end
 
+  def self.new_from_filename(filename)
+    array = filename.split(" - ")
+    artist = array[0]
+    name = array[1].gsub('.mp3','')
+    song = self.new(name)
+    song.artist = artist
+    song
+  end
+
 end
