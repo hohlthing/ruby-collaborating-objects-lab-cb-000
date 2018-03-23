43c214185ca3a661dd510cb5f58d6ee5c9dc5e27
michael hohl <mhohl@rocketmail.com>
2018-03-23 18:09:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,10 +8,11 @@ attr_accessor :name, :artist
 
   def self.new_by_filename(filename)
     array = filename.split(" - ")
+    song_name = array[1]
     artist = array[0]
-    song_name = array[1].gsub('.mp3','')
-    song = self.new(song_name)
-    song.artist = Artist.new(artist)
+    song = self.new(array[1])
+    song.artist = Artist.find_or_create_by_name(artist)
+    song.artist.add_song(song)
     song
   end
 
